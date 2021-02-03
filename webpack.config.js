25b3a6734e54f4135f9f999cae9535781815e7f9
Michael <michael.bosworth@bunchball.com>
2021-02-03 07:16:38 -0600

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -25,5 +25,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './src/index.html'
+  })]
 };
