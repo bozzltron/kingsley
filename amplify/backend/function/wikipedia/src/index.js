c54efc302641d72b6c52d82c93abe725f0228f5a
Michael <michael.bosworth@bunchball.com>
2021-02-07 15:33:17 -0600

    push
    ;
    ;

--- amplify/backend/function/wikipedia/src/index.js
+++ amplify/backend/function/wikipedia/src/index.js
@@ -2,34 +2,36 @@ const wiki = require('wikipedia');
 
 exports.handler = async (event) => {
     let response = {};
-    let defaults = {
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*"
-        }
-    };
 
     if(event.httpMethod == 'OPTIONS'){
-        return Object.assign({}, defaults, {
-            statusCode: 200,
+        return {
+            statusCode: 200,            
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
             body: {}
-        });
+        }
     }
 
     try {
         const page = await wiki.page(event.queryStringParameters['query']);
         const summary = await page.summary();
         console.log(summary);
-        response = Object.assign({}, defaults, {
+        response = {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
             body: JSON.stringify(summary),
-        });
+        };
     } catch (error) {
         console.log(error);
-        response = Object.assign({}, defaults, {
+        response = {
             statusCode: 500,
             body: error
-        });
+        };
     }
 
     return response;
--- amplify/backend/function/wolfram/src/index.js
+++ amplify/backend/function/wolfram/src/index.js
@@ -3,34 +3,36 @@ const WolframAlphaAPI = require('wolfram-alpha-api');
 
 exports.handler = async (event) => {
     let response = {};
-    let defaults = {
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*"
-        }
-    };
 
     if(event.httpMethod == 'OPTIONS'){
-        return Object.assign({}, defaults, {
+        return {
             statusCode: 200,            
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
             body: {}
-        })
+        }
     }
 
     try {
         let query = event.queryStringParameters['query'];
         const waApi = WolframAlphaAPI(process.env.API_KEY);
         let result = await waApi.getFull(query);
-        response = Object.assign({}, defaults, {
+        response = {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
             body: JSON.stringify(result),
-        });
+        };
     } catch (error) {
         console.log(error);
-        response = Object.assign({}, defaults, {
+        response = {
             statusCode: 500,
             body: error
-        })
+        }
     }
 
     return response;
